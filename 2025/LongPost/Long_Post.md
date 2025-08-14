587f1741d734dc769705a93a741420933d5f7522
snub-yeah <wil.luke985@gmail.com>
2025-08-14 10:32:03 -0500

    Added date and 404

---
+++ 2025/LongPost/Long_Post.md
@@ -0,0 +1,27 @@
+# New post has hit the snub.lol
+
+## Welcome to my longer post
+
+This post will have more words than the previous two. This is also the first post of the summer of 2025.
+
+You should be celebrating right about now.
+
+Are you enjoying it?
+
+### Here's what's gonna happen
+
+You're gonna keep reading this post.
+
+You're going to like it.
+
+You will be surprised by an image that will appear later in this post.
+
+1. Read the post
+2. Look at the image
+3. Like and subscribe
+4. ???
+5. Profit
+
+![Eikichi Onizuka](./onizuka.webp)
+
+The above is [Eikichi Onizuka](https://great-teacher-onizuka-gto.fandom.com/wiki/Eikichi_Onizuka)
--- update.py
+++ update.py
@@ -1,47 +1,63 @@
 import os
 import json
+from datetime import datetime
 
 def update_posts_json():
     json_file = 'posts.json'
-    
+
     # read existing posts
     with open(json_file, 'r') as f:
         data = json.load(f)
         existing_posts = data['posts']
-    
+
+    # add date field to existing posts that don't have it
+    for post in existing_posts:
+        if 'date' not in post:
+            # get the file path and add date from modification time
+            file_path = post['path'].lstrip('/')  # remove leading slash for os.path
+            if os.path.exists(file_path):
+                mod_time = os.path.getmtime(file_path)
+                post['date'] = datetime.fromtimestamp(mod_time).strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+
     # get existing paths to avoid duplicates
     existing_paths = {post['path'] for post in existing_posts}
-    
-    
+
+
     base_path = '2025'
     next_id = max([post['id'] for post in existing_posts], default=0) + 1
-    
+
     for root, dirs, files in os.walk(base_path):
         for file in files:
             if file.endswith('.md'):
                 # construct the full path relative to the root
                 relative_path = os.path.join('/', root, file)
                 relative_path = relative_path.replace('\\', '/')  # normalize path separators
-                
+
                 # if this path isn't in our existing posts, add it
                 if relative_path not in existing_paths:
                     # extract title from filename (removing .md extension)
                     title = os.path.splitext(file)[0]
                     formatted_title = title.replace('_', ' ')
-                    
+
+                    # get file modification time for date
+                    full_file_path = os.path.join(root, file)
+                    mod_time = os.path.getmtime(full_file_path)
+                    file_date = datetime.fromtimestamp(mod_time).strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+
                     # create new post entry
                     new_post = {
                         "id": next_id,
                         "title": formatted_title,
-                        "path": relative_path
+                        "path": relative_path,
+                        "date": file_date
                     }
-                    
+
                     existing_posts.append(new_post)
                     next_id += 1
-    
+
     # sort posts by id
     data['posts'] = sorted(existing_posts, key=lambda x: x['id'])
-    
+
     # write updated data back to file
     with open(json_file, 'w') as f:
         json.dump(data, f, indent=4)
